82e7025a29821ba1e3ab594e66091e849ff6ee81
ITP Project <krutyev6@gmail.com>
2024-03-23 14:08:25 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/CounterPage/CounterPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import s from "./CounterPage.sass"
+function CounterPage() {
+  return (
+    <div>CounterPage</div>
+  )
+}
+
+export default CounterPage
